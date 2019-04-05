abe1a88011fc20042290a5d0178e4df363da4d0a
kudaakwashe <kudaakwashe@gmail.com>
2019-04-05 23:18:31 +0200

    Features and Labels

--- Regression.py
+++ Regression.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[12]:
 
 
 import pandas as pd
+import math
 
 df = pd.read_csv("WIKI-PRICES.csv")
 print(df.head())
 
 
-# In[2]:
+# In[13]:
 
 
 df = df[['adj_open','adj_high','adj_low','adj_close','adj_volume']]
@@ -20,4 +21,29 @@ df['PCT_change'] = (df['adj_close']-df['adj_open']) / df['adj_open'] * 100.0
 df = df [['adj_close','HL_PCT','PCT_change','adj_volume']]
 
 print(df.head())
+
+
+# In[19]:
+
+
+forecast_col = 'adj_close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+print(df.head())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
